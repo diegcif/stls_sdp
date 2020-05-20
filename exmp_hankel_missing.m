358c48ccf98d6238f1e1b547e3b873180ce8a296
diego <diegcif@mit.edu>
2020-05-20 11:52:42 -0400

    add example with missing entries

--- exmp_hankel_missing.m
+++ exmp_hankel_missing.m
@@ -1,13 +1,17 @@
-% Compute the nearest rank deficient Hankel matrix
+% Nearest rank deficient Hankel matrix with missing entries
 
 % construct space of m x n hankel matrices
 m = 4;
 n = 6;
 [S,k] = hankel_struct(m,n);
 
+% missing indices
+i_miss = [m+1,k-m+1];
+
 % generate random hankel matrix
 u1 = randn(1,k);
-U1 = applyAffineMap(S,u1);
+u1(i_miss) = nan;
+U1 = hankel(u1(1:m),u1(m:k));
 disp('random hankel matrix')
 disp(U1)
 
